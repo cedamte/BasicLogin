43bc905b3f64d759680b7e4c7efe827d113f767e
dami <dami4_u@msn.com>
2020-05-03 12:09:55 +0100

    Added project gradle dependencies, and login packages

---
+++ app/src/main/java/com/example/basiclogin/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basiclogin.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.basiclogin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
