978cd8cf4fee6d210ec827a3adbc0b9d02227cac
dami <dami4_u@msn.com>
2020-05-12 10:36:43 +0100

    Validate registration input

--- app/src/main/java/com/example/basiclogin/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/basiclogin/registration/RegistrationActivity.kt
@@ -2,7 +2,11 @@ package com.example.basiclogin.registration
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.basiclogin.R
 import com.example.basiclogin.app.BasicLoginApp
 import com.example.basiclogin.home.MainActivity
@@ -11,6 +15,16 @@ import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     private lateinit var registrationViewModel: RegistrationViewModel
+    private val username by lazy {
+        findViewById<EditText>(R.id.et_username)
+    }
+    private val password by lazy {
+        findViewById<EditText>(R.id.et_password)
+    }
+
+    private val errorMessage: TextView by lazy {
+        findViewById<TextView>(R.id.tv_error_message)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +33,31 @@ class RegistrationActivity : AppCompatActivity() {
         val userManager: UserManager = (application as BasicLoginApp).userManager
         registrationViewModel = RegistrationViewModel(userManager = userManager)
 
+        registrationViewModel.registrationState.observe(this, Observer { regState ->
+            when (regState) {
+                is RegistrationViewState.RegistrationSuccess -> {
+                    val username = username.text.toString()
+                    val password = password.text.toString()
+                    registrationViewModel.registerUser(username, password)
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+                is RegistrationViewState.RegistrationError -> {
+                    errorMessage.text = regState.errorMessage
+                    errorMessage.visibility = View.VISIBLE
+                }
+            }
+        })
+
         btn_sign_up.setOnClickListener {
             val username = et_username.text.toString()
             val password = et_password.text.toString()
-            registrationViewModel.registerUser(username, password)
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            registrationViewModel.validateInput(username, password)
         }
     }
+}
+
+sealed class RegistrationViewState {
+    object RegistrationSuccess : RegistrationViewState()
+    data class RegistrationError(val errorMessage: String) : RegistrationViewState()
 }
--- app/src/main/java/com/example/basiclogin/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/basiclogin/registration/RegistrationViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.basiclogin.registration
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.basiclogin.user.UserManager
+
+
+private const val MIN_INPUT_LENGTH = 5
 
 class RegistrationViewModel
     (
     private val userManager: UserManager
 ) : ViewModel() {
+
+
+    private val _registrationState = MutableLiveData<RegistrationViewState>()
+    val registrationState: LiveData<RegistrationViewState>
+        get() = _registrationState
 
     fun registerUser(
         username: String,
@@ -17,5 +27,18 @@ class RegistrationViewModel
             username,
             password
         )
+    }
+
+    fun validateInput(
+        username: String,
+        password: String
+    ) {
+        when {
+            username.length < MIN_INPUT_LENGTH -> _registrationState.value =
+                RegistrationViewState.RegistrationError("Username has to be longer than 4 Characters")
+            password.length < MIN_INPUT_LENGTH -> _registrationState.value =
+                RegistrationViewState.RegistrationError("Password has to be longer than 4 Characters")
+            else -> _registrationState.value = RegistrationViewState.RegistrationSuccess
+        }
     }
 }
