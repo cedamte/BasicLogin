10a9b9cdb2062d37b7e5c88d9fc0217654b6a2ae
dami <dami4_u@msn.com>
2020-05-12 10:43:05 +0100

    Use SharedPreferences as application storage

--- app/src/main/java/com/example/basiclogin/app/BasicLoginApp.kt
+++ app/src/main/java/com/example/basiclogin/app/BasicLoginApp.kt
@@ -1,13 +1,14 @@
 package com.example.basiclogin.app
 
 import android.app.Application
+import com.example.basiclogin.storage.SharedPreferencesStorage
 import com.example.basiclogin.user.UserManager
 import timber.log.Timber
 
 open class BasicLoginApp : Application() {
 
     open val userManager: UserManager by lazy {
-        UserManager()
+        UserManager(SharedPreferencesStorage(this))
     }
 
     override fun onCreate() {
